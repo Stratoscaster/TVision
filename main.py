bc29a673529f5173c16e226b72011533a319e702
Stratos <stratoscaster191@gmail.com>
2022-12-10 16:37:27 -0800

    Fixed neopixel pixel format (was GRB, changed to RGB)

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ while keypressed != ord('q'):
     if isinstance(frame, np.ndarray):
         # grab first row of pixels
         frame = cap.next_frame()
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        # frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
         result = frame_con.process_frame_and_update_rgb(frame)
         cv.imshow('resultant frame', cv.resize(frame, (height * 3, width * 3)))
         print('frame initial', frame[0][0])
